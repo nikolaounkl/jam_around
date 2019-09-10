9409ee66523491123226db53070f5a3ec86fddb5
nikolaounkl <nikolaounkl@gmail.com>
2019-09-10 21:12:22 +0200

    Updates

---
+++ precache-manifest.4734a5697fe44b64cba064c2939c9f1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84b7821af49116cd691a985aee39ec9",
+    "url": "/jam_around/index.html"
+  },
+  {
+    "revision": "6bf7b18c873ca7e3ef3a",
+    "url": "/jam_around/static/css/main.bdee24f6.chunk.css"
+  },
+  {
+    "revision": "a1cfaa68a8db72d87bab",
+    "url": "/jam_around/static/js/2.98e5a7a5.chunk.js"
+  },
+  {
+    "revision": "6bf7b18c873ca7e3ef3a",
+    "url": "/jam_around/static/js/main.6b45c041.chunk.js"
+  },
+  {
+    "revision": "3bb1676160cc59d9cf78",
+    "url": "/jam_around/static/js/runtime~main.fed4dd2b.js"
+  }
+]);
